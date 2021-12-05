5031e60590ffac75f40c90f92dd05b0f66ae813f
v-Wint <42693472+v-Wint@users.noreply.github.com>
2021-12-05 12:43:06 +0200

    Add files via upload

---
+++ Lab 6/Lab6.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int combination(int m, int n);
+
+int main()
+{
+    int m, n, result;
+
+    cout << "Enter m: "; cin >> m;
+    cout << "Enter n: "; cin >> n;
+
+    result = combination(m, n);
+
+    printf("Combination of n=%d m=%d is %d", n, m, result);
+}
+
+int combination(int m, int n) {
+    int res;
+    if (m > n && n >= 0) {
+        res = 0;
+    }
+    else {
+        if ((m == 0 && n > 0) || (m == n && n >= 0)) {
+            res = 1;
+        }
+        else {
+            res = combination(m - 1, n - 1) + combination(m, n - 1);
+        }
+    }
+    return res;
+}
